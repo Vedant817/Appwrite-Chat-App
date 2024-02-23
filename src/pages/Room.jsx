4cb563c0e5f89531baaa9e3f2a5d3ab71f01ce6f
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-02-23 10:49:55 +0530

    Adding the New chats to the database and displaying the same

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,30 +1,74 @@
 import { useState, useEffect } from 'react'
 import { databases, DATABASE_ID, COLLECTION_ID_MESSAGE } from '../appwriteConfig';
+import { ID, Query } from 'appwrite';
 
 
 const Room = () => {
+  const [messages, setMessages] = useState([]);
+  const [messageBody, setMessageBody] = useState('')
 
-  const [messages, setMessages] = useState([])
   useEffect(() => {
     getMessages()
   }, [])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const payload = {
+      body: messageBody
+    }
+
+    let response = await databases.createDocument(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGE,
+      ID.unique(),
+      payload
+    )
+    console.log('Created!! ', response);
+    setMessages(prevState => [response, ...messages])
+    setMessageBody('')
+  }
+
   const getMessages = async () => {
-    const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID_MESSAGE);
-    console.log('RESPONSE: ', response);
-    setMessages(response.documents)
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        COLLECTION_ID_MESSAGE,
+        //TODO: Adding the Query for Fetching the Messages in order.
+        // [
+        //   Query.orderDesc("$createdAt"),
+        //   Query.limit(100)
+        // ]
+      )
+      console.log('RESPONSE: ', response);
+      setMessages(response.documents)
+    } catch (error) {
+      alert('Error Fetching the Messages!!', error)
+    }
   }
 
   return (
     <main className='container'>
       <div className='room--container'>
+
+        <form id='message--form' onSubmit={handleSubmit}>
+          <div>
+            <textarea required maxLength='1000' placeholder='Say Something...' onChange={(e) => { setMessageBody(e.target.value) }} value={messageBody} />
+          </div>
+          <div className='send-btn--wrapper'>
+            <input className='btn btn--secondary' type='submit' value='Send' />
+          </div>
+        </form>
+
         <div>
           {messages.map((message) => (
             <div key={message.$id} className='message--wrapper'>
               <div className='message--header'>
-                <p className='message-timestamp'>{message.$createdAt}</p>
+                <small className='message-timestamp'>{message.$createdAt}</small>
               </div>
-              <span>{message.body}</span>
+              <div className='message--body'>
+                <span>{message.body}</span>
+              </div>
             </div>
           ))}
         </div>

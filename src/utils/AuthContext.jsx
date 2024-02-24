dd7961c75db1ecffadab666f54036e3532f63a4d
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-02-25 01:20:13 +0530

    Completed Login User

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -1,21 +1,36 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import {account} from '../appwriteConfig';
+import {useNavigate} from 'react-router-dom'
 
 const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
     const [loading, setLoading] = useState(true)
     const [user, setUser] = useState(null)
+    const navigate = useNavigate()
 
     useEffect(() => {
-        setLoading(false)
+        getUserOnLoad()
     }, [])
+
+    const getUserOnLoad = async ()=>{
+        try {
+            const accountDetails = account.get();
+            setUser(accountDetails)
+        } catch (error) {
+            console.error(error);
+        }
+        setLoading(false)
+    }
 
     const handleUserLogin = async (e, credentials) => {
         e.preventDefault();
         try {
             const response = await account.createEmailPasswordSession(credentials.email, credentials.password);
             console.log(response);
+            const accountDetails = account.get();
+            setUser(accountDetails);
+            navigate('/')
         } catch (error) {
             console.log(error);
         }

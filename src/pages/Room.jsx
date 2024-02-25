b82efe674ac9ff7b46847cdcb0752032a54ac858
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-02-25 14:28:42 +0530

    Adding Document Level permissions to the messages

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from 'react'
 import client, { databases, DATABASE_ID, COLLECTION_ID_MESSAGE } from '../appwriteConfig';
-import { ID, Query } from 'appwrite';
+import { ID, Query, Role, Permission } from 'appwrite';
 import { Trash2 } from 'react-feather';
 import Header from '../components/Header';
 import { useAuth } from '../utils/AuthContext';
 
 
 const Room = () => {
-  const {user} = useAuth()
+  const { user } = useAuth()
 
   const [messages, setMessages] = useState([]);
   const [messageBody, setMessageBody] = useState('')
@@ -16,12 +16,12 @@ const Room = () => {
   useEffect(() => {
     getMessages();
 
-    const unsubscribe = client.subscribe(`databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGE}.documents`,response => { //! Helps in building a real time connection so that changes at one can be reflected at the other too.
-      if(response.events.includes("databases.*.collections.*documents.*create")){
+    const unsubscribe = client.subscribe(`databases.${DATABASE_ID}.collections.${COLLECTION_ID_MESSAGE}.documents`, response => { //! Helps in building a real time connection so that changes at one can be reflected at the other too.
+      if (response.events.includes("databases.*.collections.*documents.*create")) {
         console.log('MESSAGE WAS CREATED');
         setMessages(prevState => [response.payload, ...prevState])
       }
-      if(response.events.includes("databases.*.collections.*.documents.*.delete")){
+      if (response.events.includes("databases.*.collections.*.documents.*.delete")) {
         console.log('MESSAGE WAS DELETED');
         setMessages(prevState => prevState.filter(message => message.$id !== response.payload.$id))
       }
@@ -40,11 +40,16 @@ const Room = () => {
       body: messageBody
     }
 
+    let permissions = [
+      Permission.write(Role.user(user.$id)) //! This allows only the current user to make changes into the messages sent.
+    ]
+
     let response = await databases.createDocument(
       DATABASE_ID,
       COLLECTION_ID_MESSAGE,
       ID.unique(),
-      payload
+      payload,
+      permissions
     )
     console.log('Created!! ', response);
     setMessages(prevState => [response, ...messages])
@@ -97,9 +102,11 @@ const Room = () => {
                   ) : (
                     <span>Anonymous User</span>
                   )}
-                <small className='message-timestamp'>{new Date(message.$createdAt).toLocaleString()}</small>
+                  <small className='message-timestamp'>{new Date(message.$createdAt).toLocaleString()}</small>
                 </p>
-                <Trash2 onClick={()=>{deleteMessage(message.$id)}} className='delete--btn' />
+                {message.$permissions.includes(`delete("user:${user.$id}")`) && (
+                  <Trash2 onClick={() => { deleteMessage(message.$id) }} className='delete--btn' />
+                )}
               </div>
               <div className='message--body'>
                 <span>{message.body}</span>
